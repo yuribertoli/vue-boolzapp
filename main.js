ba6b257a956c482a987f766b4c06686c8f726a8a
yuribertoli <yuribertoli@yahoo.it>
2022-02-15 17:23:37 +0100

    toggle show settings

--- main.js
+++ main.js
@@ -18,16 +18,19 @@ const app = new Vue ({
                 visible: true,
                 messages: [
                     {
+                    setting: false,
                     date: '10/01/2020 15:30:55',
                     text: 'Hai portato a spasso il cane?',
                     status: 'sent'
                     },
                     {
+                    setting: false,
                     date: '10/01/2020 15:50:00',
                     text: 'Ricordati di dargli da mangiare',
                     status: 'sent'
                     },
                     {
+                    setting: false,
                     date: '10/01/2020 16:15:22',
                     text: 'Tutto fatto!',
                     status: 'received'
@@ -41,16 +44,19 @@ const app = new Vue ({
                 visible: true,
                 messages: [
                     {
+                    setting: false,
                     date: '20/03/2020 16:30:00',
                     text: 'Ciao come stai?',
                     status: 'sent'
                     },
                     {
+                    setting: false,
                     date: '20/03/2020 16:30:55',
                     text: 'Bene grazie! Stasera ci vediamo?',
                     status: 'received'
                     },
                     {
+                    setting: false,
                     date: '20/03/2020 16:35:00',
                     text: 'Mi piacerebbe ma devo andare a fare la spesa.',
                     status: 'sent'
@@ -64,16 +70,19 @@ const app = new Vue ({
                 visible: true,
                 messages: [
                     {
+                    setting: false,
                     date: '28/03/2020 10:10:40',
                     text: 'La Marianna va in campagna',
                     status: 'received'
                     },
                     {
+                    setting: false,
                     date: '28/03/2020 10:20:10',
                     text: 'Sicuro di non aver sbagliato chat?',
                     status: 'sent'
                     },
                     {
+                    setting: false,
                     date: '28/03/2020 16:15:22',
                     text: 'Ah scusa!',
                     status: 'received'
@@ -87,11 +96,13 @@ const app = new Vue ({
                 visible: true,
                 messages: [
                     {
+                    setting: false,
                     date: '10/01/2020 15:30:55',
                     text: 'Lo sai che ha aperto una nuova pizzeria?',
                     status: 'sent'
                     },
                     {
+                    setting: false,
                     date: '10/01/2020 15:50:00',
                     text: 'Si, ma preferirei andare al cinema',
                     status: 'received'
@@ -134,6 +145,10 @@ const app = new Vue ({
                 }, 1000);
 
             }       
+        },
+
+        showSettings(indice) {
+            this.contacts[this.attivo].messages[indice].setting = !this.contacts[this.attivo].messages[indice].setting;
         }
     },
 });
